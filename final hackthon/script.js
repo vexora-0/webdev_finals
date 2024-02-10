062df057c54050b354136592fcc2af0d8d4461f7
retrorep07 <143892094+giga-tes@users.noreply.github.com>
2024-02-10 16:18:28 +0530

    Update script.js

--- final hackthon/script.js
+++ final hackthon/script.js
@@ -31,30 +31,6 @@ console.log("connected");
                         <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679" alt="#">
                     </div>
                 </div>
-            </div>
-            
-            <div class="comment-post">
-                <div class="prof-img">
-                        <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/031/original/profile_image.png?1706888739" alt="#">
-                    </div>
-                    <div class="post-right">    
-                        <div class="post-ofright">
-                            <h4>Name</h4>
-                            <h5>@user</h5>
-                            <div class="post-right-btns">
-                                <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/028/original/edit.png?1706888661" alt="#">
-                                <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/027/original/delete.png?1706888643" alt="#">
-                            </div>
-                        </div>
-                    
-                        <div class="post-txt-area">
-                            <textarea name="post" id="post-area-text" cols="30" rows="10" style="resize: none;"></textarea>
-                        </div>
-                    
-                        <div class="like-cmnt-btns">
-                            <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/029/original/heart.png?1706888679" alt="#">
-                        </div>
-                    </div>
                 </div>
             </div>`
             posts_box.appendChild(main_post);
